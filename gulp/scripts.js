30519e21b28c4cc2c5c8836a3ba11cec6d13c455
Travis Tidwell <travis@form.io>
2016-01-25 11:20:15 -0600

    Adding the ResourceProvider to the template.

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -21,6 +21,5 @@ gulp.task('scripts', function() {
 function buildScripts() {
   return gulp.src(path.join(conf.paths.src, '/app/**/*.js'))
     .pipe($.eslint())
-    .pipe($.eslint.format())
     .pipe($.size())
 };
