9e2925fb7be922a97937ec7f261810f8775846d4
Travis Tidwell <travis@form.io>
2016-09-12 11:10:34 -0700

    Update dependencies.

--- gulp/build.js
+++ gulp/build.js
@@ -72,9 +72,10 @@ gulp.task('html', ['inject', 'partials'], function () {
 // Only applies for fonts from bower dependencies
 // Custom fonts are handled by the "other" task
 gulp.task('fonts', function () {
-  return gulp.src($.mainBowerFiles())
+  return gulp.src([
+    'bower_components/bootstrap-sass/assets/fonts/**/*'
+  ])
     .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
-    .pipe($.flatten())
     .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));
 });
 
